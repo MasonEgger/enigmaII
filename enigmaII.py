f7de665dcb67e960303ff8402c683c3c87f17422
Mason Egger <mason@phatsoundsmedia.com>
2016-10-27 20:03:49 -0500

    cleaning up main

--- enigmaII.py
+++ enigmaII.py
@@ -1,3 +1,5 @@
+import re
+import sys
 wheelV = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 wheelVI = ['a', 'c', 'e', 'd', 'f', 'h', 'g', 'i', 'k', 'j', 'l', 'n', 'm', 'o', 'q', 'p', 'r', 't', 's', 'u', 'w', 'v', 'x', 'z', 'y', 'b']
 wheelVII = ['a', 'z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b']
@@ -31,11 +33,27 @@ def encrypt(order, code, message):
         else:
             num_letters += 1
 
-    return enc_message
+    return enc_message.upper()
 
 
-order = raw_input("Enter order: ")
-code = raw_input("Enter the code: ")
-message = raw_input("Enter the messae to encrypt: ")
+if __name__ == "__main__":
+    alpha_regex = re.compile('[^a-zA-Z]')
 
-print encrypt(order.lower(), code.lower(), message.lower()).upper()
+    # get and validate order
+    order = raw_input("Enter order: ")
+    if len(order) != 3:
+        print "Invalid order"
+        sys.exit(1)
+
+    # get and validate code
+    code = raw_input("Enter the code: ")
+    code = alpha_regex.sub('', code).lower()
+    if len(code) != 3:
+        print "Invalid code"
+        sys.exit(1)
+
+    # get and sanitate message to encrypt
+    message = raw_input("Enter the messae to encrypt: ")
+    message = alpha_regex.sub('', message).lower()
+
+    print encrypt(order.lower(), code.lower(), message)
