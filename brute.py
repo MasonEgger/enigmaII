968ecfe9b2b7076ed3d2af9cd2e1a3f784546806
Mason Egger <megger@forcepoint.com>
2016-10-30 20:50:47 -0500

    adding .gitignore and using itertools.product on key/order generators

--- brute.py
+++ brute.py
@@ -14,12 +14,11 @@ if __name__ == "__main__":
 
     key = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     order = ['5', '6', '7']
-    order_perms = list(itertools.permutations(order))
-    key_perms = list(itertools.combinations_with_replacement(key, 3))
+    order_perms = itertools.permutations(order)
+    key_perms = itertools.combinations_with_replacement(key, 3)
     alpha_regex = re.compile('[^a-zA-Z]')
 
     message = alpha_regex.sub('', args.message).lower()
 
-    for x in order_perms:
-        for y in key_perms:
-            print "ORDER: %s KEY: %s MESSAGE: %s" % (x, y, decrypt(x, y, message).replace(" ", ""))
+    for x, y in itertools.product(order_perms, key_perms):
+        print "ORDER: %s KEY: %s MESSAGE: %s" % (x, y, decrypt(x, y, message).replace(" ", ""))
